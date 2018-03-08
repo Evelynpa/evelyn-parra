2723be190daea3323ee53d95a97ffe7133b1a8d5
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-03-08 08:32:22 -0300

    se hacen cambios visuales

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,10 @@
 
 })();
 
-
 function parallax(){
     let layer_1 = document.getElementById('layer_1');
-    let layer_2 = document.getElementById('layer_2');
 
     // aplicar posicion
-    layer_1.style.top = -(window.pageYOffset / 8) + 'px';
-    layer_2.style.top = -(window.pageYOffset / 6) + 'px';
+    layer_1.style.top = -(window.pageYOffset / 2) + 'px';
 }
 window.addEventListener('scroll', parallax, false);
-
-$(".foo, .bar").smoove({
-    offset  : '15%',
-    // moveX is overridden to -200px for ".bar" object
-    moveX   : '100px',
-    moveY   : '100px',
-  });
