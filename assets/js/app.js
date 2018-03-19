c2b9a3ce17dc1c9a296cf4c41cf4a10f8de76121
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-03-19 01:58:08 -0300

    se actualiza portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,50 @@
-(function load(){
+(function load() {
 
 })();
 
-function parallax(){
-    let layer_1 = document.getElementById('layer_1');
+// function parallax() {
+//     let layer_1 = document.getElementById('layer_1');
 
-    // aplicar posicion
-    layer_1.style.top = -(window.pageYOffset / 2) + 'px';
-}
-window.addEventListener('scroll', parallax, false);
+//     // aplicar posicion
+//     layer_1.style.top = -(window.pageYOffset / 2) + 'px';
+// }
+
+
+// window.addEventListener('scroll', parallax, false);
+
+
+
+
+
+$(window).scroll(function () {
+    $scrol = $(document).scrollTop();
+    console.log($scrol);
+    if ($scrol > 60) {
+        $('.navbar').addClass('shrink');
+        $('.navbar').addClass('navbar-fixed-top');
+    }
+    else {
+        $('.navbar').removeClass('shrink');
+    }
+
+
+
+
+});
+
+$('.nav li a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) + ']');
+        if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body').animate({ scrollTop: targetOffset }, 800);
+            return false;
+        }
+    }
+});
+
+
+
+
